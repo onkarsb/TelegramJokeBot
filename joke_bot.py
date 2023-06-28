56745ffa1d96f1f5a0bf665cd968c65344b5de44
onkarsb <er.onkarbadiger@gmail.com>
2023-06-29 00:15:05 +0530

    Added /start command handler
    Signed-off-by: onkarsb <er.onkarbadiger@gmail.com>

--- joke_bot.py
+++ joke_bot.py
@@ -2,22 +2,29 @@ import requests
 from telegram.ext import CommandHandler, Updater
 from joke_module import TOKEN, JOKE_API_URL
 
-
-
+# Define a function to handle the /start command
+def start(update, context):
+    # Send a welcome message
+    update.message.reply_text('Welcome to Joke Bot. Send /joke to get a joke')
+    chat_id=update.effective_chat.id
+    chat_username = update.effective_chat.username
+    print(f"Start cmd\nUsername : {chat_username}\nChat Id : {chat_id}")
+    
 # Define the function to handle the /joke command
-def send_joke(update, context):
+def get_joke(update, context):
     # Send a request to the joke API
     response = requests.get(JOKE_API_URL)
     if response.status_code == 200:
         joke = response.text.strip()
         # Send the joke as a message to the user
         chat_id=update.effective_chat.id
+        chat_username = update.effective_chat.usernamechat_id=update.effective_chat.id
         chat_username = update.effective_chat.username
         context.bot.send_message(chat_id=chat_id, text=joke)
-        print(f"Username : {chat_username}\nChat Id : {chat_id}\nJoke: \n {joke}")
+        print(f"Joke cmd\nUsername : {chat_username}\nChat Id : {chat_id}\nJoke: \n {joke}")
     else:
         context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I couldn't fetch a joke at the moment. Please try again later.")
-
+    
 def main():
     # Create an instance of the Updater class and pass in your bot's token
     updater = Updater(TOKEN)
@@ -25,8 +32,11 @@ def main():
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
 
+    # Register the start function as a command handler
+    dispatcher.add_handler(CommandHandler('start', start))
+
     # Register the /joke command handler
-    dispatcher.add_handler(CommandHandler('joke', send_joke))
+    dispatcher.add_handler(CommandHandler('joke', get_joke))
 
     # Start the bot
     print("Bot starting...")
--- joke_module.py
+++ joke_module.py
@@ -1,5 +1,5 @@
 # Define the Telegram bot token
-TOKEN = '6255139505:AAGQXZwzi4xH1KPI3UAIPwQ6OcmSvojSayE'
+TOKEN = '6255139505:AAGQXZwzi4xH1KPI3UAIPwQ6OcmSvojSayE' #Paste your token that you get from BotFather
 
 # Define the URL for retrieving jokes
 JOKE_API_URL = 'https://v2.jokeapi.dev/joke/Any?format=txt'
