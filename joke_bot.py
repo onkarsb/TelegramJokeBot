e8e30c41ec3978d35e493835d5511743ab538e40
Prajval Baidger <er.prajvalbadiger@gmail.com>
2023-12-28 15:20:01 +0530

    removed joke_module.py

--- joke_bot.py
+++ joke_bot.py
@@ -1,6 +1,17 @@
 import requests
 from telegram.ext import CommandHandler, Updater
-from joke_module import TOKEN, JOKE_API_URL
+import os
+
+# Define the Telegram bot token
+# export your token that you get from BotFather
+# Run the below commands in the terminal
+# export TOKEN= "6255139505:AAGQXZwzi4xH1KPI3UAIPwQ6OcmSvojSayE"
+# export JOKE_API_URL="https://v2.jokeapi.dev/joke/Any?format=txt"
+
+# TOKEN and JOKE_API_URL should be exported before running the script
+TOKEN = os.environ['TOKEN']
+# Define the URL for retrieving jokes
+JOKE_API_URL = os.environ['JOKE_API_URL']
 
 
 # Define a function to handle the /start command
